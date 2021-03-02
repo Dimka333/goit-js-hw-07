20749aac1acb31efbe209ae529a9879f9599325f
Dmytro Kuzminskyi <Dimka333@users.noreply.github.com>
2021-03-02 18:09:44 +0200

    ДЗ js №7
    Исправлены ошибки.

--- js/task-number-8.js
+++ js/task-number-8.js
@@ -1,32 +1,64 @@
+// const inputRef = document.querySelector('#controls > input');
+// const renderRef = document.querySelector('button[data-action="render"]');
+// const destroyRef = document.querySelector('button[data-action="destroy"]');
+// const divBoxes = document.querySelector('#boxes');
+// let randomColor = '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase();
+// let sizeBox = 30;
+
+
+
+// function createBoxes(amount) {
+//     amount = Number(inputRef.value);
+//     if (amount > 0) {
+        
+// let newDiv = document.createElement('div');
+//     newDiv.style.backgroundColor = randomColor;
+//     newDiv.style.width = sizeBox + 'px';
+//     newDiv.style.height = sizeBox + 'px';
+//     sizeBox += 10;
+//         newDiv.appendChild(divBoxes);
+//         amount = amount - 1;
+//     }
+//     else console.log('Дивы кончились =(');
+        
+// };
+
+// function destroyBoxes() {
+//     while (divBoxes.firstChild) {
+//         divBoxes.removeChild(divBoxes.firstChild)
+//     }
+// };
+
+// renderRef.addEventListener('click', createBoxes);
+// destroyRef.addEventListener('click', destroyBoxes);
+
+// console.log(divBoxes);
+
+
+
 const inputRef = document.querySelector('#controls > input');
 const renderRef = document.querySelector('button[data-action="render"]');
 const destroyRef = document.querySelector('button[data-action="destroy"]');
 const divBoxes = document.querySelector('#boxes');
-let randomColor = '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase();
-let sizeBox = 30;
-
-function createBoxes(amount) {
-    amount = Number(inputRef.value);
-    if (amount > 0) {
-let newDiv = document.createElement('div');
+function createBoxes() {
+    let sizeBox = 30;
+    let startValue = 1
+    while (+inputRef.value >= startValue ) {
+    let randomColor = '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase();
+    startValue +=1
+    let newDiv = document.createElement('div');
     newDiv.style.backgroundColor = randomColor;
     newDiv.style.width = sizeBox + 'px';
     newDiv.style.height = sizeBox + 'px';
     sizeBox += 10;
-        newDiv.appendChild(divBoxes);
-        amount = amount - 1;
+    divBoxes.appendChild(newDiv);
+    // amount = amount - 1;
     }
-    else console.log('Дивы кончились =(');
-        
+    console.log('Дивы кончились =(');
 };
-
 function destroyBoxes() {
-    while (divBoxes.firstChild) {
-        divBoxes.removeChild(divBoxes.firstChild)
-    }
+    divBoxes.innerHTML = ''
+    inputRef.value = ''
 };
-
 renderRef.addEventListener('click', createBoxes);
 destroyRef.addEventListener('click', destroyBoxes);
-
-console.log(inputRef.value);
